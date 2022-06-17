c0d1de361e251532e1424796bab7b4aace8ffbe7
EC2 Default User <ec2-user@ip-10-100-51-126.ec2.internal>
2022-06-17 21:33:30 +0000

    Small nlp steps.

--- get_data.sh
+++
@@ -1,6 +0,0 @@
-mkdir dogs
-cd dogs
-wget http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar
-wget http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar
-tar -xvf images.tar 
-tar -xvf annotation.tar
